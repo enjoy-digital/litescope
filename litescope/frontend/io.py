9393fee9f3f10a08e68da659b3e4d54b5586ad34
Florent Kermarrec <florent@enjoy-digital.fr>
2015-09-07 11:49:54 +0200

    init repo

---
+++ litescope/frontend/io.py
@@ -0,0 +1,11 @@
+from litescope.common import *
+
+
+class LiteScopeIO(Module, AutoCSR):
+    def __init__(self, dw):
+        self.dw = dw
+        self._input = CSRStatus(dw)
+        self._output = CSRStorage(dw)
+
+        self.i = self._input.status
+        self.o = self._output.storage
