a80d890fb922158dbf98954a1939972a5ce0f5b7
Florent Kermarrec <florent@enjoy-digital.fr>
2017-01-19 05:30:25 +0100

    test/test_dump: cleanup

--- test/test_dump.py
+++ test/test_dump.py
@@ -1,28 +1,39 @@
 import unittest
+import os
+from math import cos, sin
 
 from litescope.software.dump import *
 
+#TODO:
+# - find a way to check if files are generated corectly
+
+dump = Dump()
+for i in range(4):
+    dump.add(DumpVariable("ramp"+str(i), 2**i, [j for j in range(256)]))
+pi = 3.1415
+dump.add(DumpVariable("sin", 8, [128+128*sin(j/(2*pi*16)) for j in range(1024)]))
+dump.add(DumpVariable("cos", 8, [128+128*cos(j/(2*pi*16)) for j in range(1024)]))
+
+
 class TestDump(unittest.TestCase):
-    def test(self):
-        print("creating dump...")
-        dump = Dump()
-        for i in range(4):
-            dump.add(DumpVariable("foo"+str(i), 2**i, [j for j in range(256)]))
-        pi = 3.1415
-        from math import cos, sin
-        dump.add(DumpVariable("sinus", 8, [128+128*sin(j/(2*pi*16)) for j in range(1024)]))
-        dump.add(DumpVariable("cosinus", 8, [128+128*cos(j/(2*pi*16)) for j in range(1024)]))
+    def test_csv(self):
+        filename = "dump.csv"
+        CSVDump(dump).write(filename)
+        os.remove(filename)
 
-        print("csv export test")
-        CSVDump(dump).write("dump.csv")
+    def test_py(self):
+        filename = "dump.py"
+        PythonDump(dump).write(filename)
+        os.remove(filename)
 
-        print("python export test...")
-        PythonDump(dump).write("dump.py")
+    def test_sigrok(self):
+        filename = "dump.sr"
+        SigrokDump(dump).write(filename)
+        SigrokDump(dump).read(filename)
+        SigrokDump(dump).write(filename)
+        os.remove(filename)
 
-        print("sigrok export/import test...")
-        SigrokDump(dump).write("dump.sr")
-        SigrokDump(dump).read("dump.sr")
-        SigrokDump(dump).write("dump.sr")
-
-        print("vcd export test...")
-        VCDDump(dump).write("dump.vcd")
+    def test_vcd(self):
+        filename = "dump.vcd"
+        VCDDump(dump).write(filename)
+        os.remove(filename)
