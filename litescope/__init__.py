985585f0b9ecb820df48adc5ad6c64423a2fe236
Florent Kermarrec <florent@enjoy-digital.fr>
2018-02-19 14:48:24 +0100

    __init__: add LiteScopeIODriver and LiteScopeAnalyzerDriver imports

--- litescope/__init__.py
+++ litescope/__init__.py
@@ -1,1 +1,3 @@
 from litescope.core import LiteScopeIO, LiteScopeAnalyzer
+from litescope.software.driver.io import LiteScopeIODriver
+from litescope.software.driver.analyzer import LiteScopeAnalyzerDriver
