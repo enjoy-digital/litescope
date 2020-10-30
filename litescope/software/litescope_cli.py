94e2d15c9490bb29ef9e5cdac7fda374a6b34d5a
Florent Kermarrec <florent@enjoy-digital.fr>
2020-10-30 11:58:38 +0100

    software/litescope_cli: cleanup, use --csv to select analyzer file and add --dump argument.

--- litescope/software/litescope_cli.py
+++ litescope/software/litescope_cli.py
@@ -7,6 +7,7 @@
 # Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
+import os
 import re
 import csv
 import sys
@@ -32,11 +33,11 @@ class Finder:
 
     def __getitem__(self, name):
         scores = {s: 0 for s in self.signals}
-        # exact match
+        # Exact match
         if name in scores:
             print("Exact:", name)
             return name
-        # substring
+        # Substring
         pattern = re.compile(name)
         max_score = 0
         for s in self.signals:
@@ -49,29 +50,8 @@ class Finder:
         name, score = best[0]
         return name
 
-# Main script --------------------------------------------------------------------------------------
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="""Run LiteScope with selected triggers
-
-    Allows to add triggers based on signal names. Supports simple substring matching
-    and Python regular expressions.
-    """)
-    parser.add_argument("-r", "--rising-edge",   action="append",          help="Add rising edge trigger")
-    parser.add_argument("-f", "--falling-edge",  action="append",          help="Add falling edge trigger")
-    parser.add_argument("-v", "--value-trigger", action="append", nargs=2, help="Add conditional trigger with given value",
-                        metavar=("TRIGGER", "VALUE"))
-    parser.add_argument("-l", "--list",          action="store_true",      help="List signal choices")
-    parser.add_argument("--name",                default="analyzer",       help="Analyzer Name.")
-    parser.add_argument("--group",               default="0",              help="Capture Group.")
-    parser.add_argument("--subsampling",         default="1",              help="Capture Subsampling.")
-    parser.add_argument("--offset",              default="32",             help="Capture Offset.")
-    parser.add_argument("--length",              default=None,             help="Capture Length.")
-    args = parser.parse_args()
-    return args
-
 def add_triggers(args, analyzer, signals):
-    added = False
+    added  = False
     finder = Finder(signals)
 
     for signal in args.rising_edge or []:
@@ -94,35 +74,65 @@ def add_triggers(args, analyzer, signals):
         added = True
     return added
 
+# Main ---------------------------------------------------------------------------------------------
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="""LiteScope Client utility""")
+    parser.add_argument("-r", "--rising-edge",   action="append",          help="Add rising edge trigger")
+    parser.add_argument("-f", "--falling-edge",  action="append",          help="Add falling edge trigger")
+    parser.add_argument("-v", "--value-trigger", action="append", nargs=2, help="Add conditional trigger with given value",
+        metavar=("TRIGGER", "VALUE"))
+    parser.add_argument("-l", "--list",          action="store_true",      help="List signal choices")
+    parser.add_argument("--csv",                 default="analyzer.csv",   help="Analyzer CSV file")
+    parser.add_argument("--group",               default="0",              help="Capture Group")
+    parser.add_argument("--subsampling",         default="1",              help="Capture Subsampling")
+    parser.add_argument("--offset",              default="32",             help="Capture Offset")
+    parser.add_argument("--length",              default=None,             help="Capture Length")
+    parser.add_argument("--dump",                default="dump.vcd",       help="Capture Filename")
+    args = parser.parse_args()
+    return args
+
 def main():
     args = parse_args()
 
-    signals = get_signals(args.name + ".csv", args.group)
+    basename = os.path.splitext(os.path.basename(args.csv))[0]
+
+    # Check if analyzer file is present and exit if not.
+    if not os.path.exists(args.csv):
+        raise ValueError("{} not found, exiting.".format(args.csv))
+        sys.exit(1)
+
+    # Get list of signals from analyzer configuratio file.
+    signals = get_signals(args.csv, args.group)
+
+    # If in list mode, list signals and exit.
     if args.list:
         for signal in signals:
             print(signal)
         sys.exit(0)
 
-    wb = RemoteClient()
-    wb.open()
+    # Create and open remote control.
+    bus = RemoteClient()
+    bus.open()
 
+    # Configure and run LiteScope analyzer.
     try:
-        analyzer = LiteScopeAnalyzerDriver(wb.regs, args.name, debug=True)
+        analyzer = LiteScopeAnalyzerDriver(bus.regs, basename, debug=True)
         analyzer.configure_group(int(args.group, 0))
         analyzer.configure_subsampler(int(args.subsampling, 0))
         if not add_triggers(args, analyzer, signals):
-            print("WARNING: no trigger added!")
-
+            print("No trigger, immediate capture.")
         analyzer.run(
             offset = int(args.offset, 0),
             length = None if args.length is None else int(args.length, 0)
         )
-
         analyzer.wait_done()
         analyzer.upload()
-        analyzer.save("dump.vcd")
+        analyzer.save(args.dump)
+
+    # Close remove control.
     finally:
-        wb.close()
+        bus.close()
 
 if __name__ == "__main__":
     main()
