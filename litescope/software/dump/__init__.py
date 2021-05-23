1243ab3c81fc33ddacb3c15d18b6c63a45e6989e
Arnaud Durand <arnaud.durand@unifr.ch>
2021-05-24 01:21:35 +0200

    software/dump: add JSON dump

--- litescope/software/dump/__init__.py
+++ litescope/software/dump/__init__.py
@@ -1,5 +1,6 @@
 from litescope.software.dump.common import DumpData, DumpVariable, Dump
 from litescope.software.dump.csv import CSVDump
 from litescope.software.dump.python import PythonDump
+from litescope.software.dump.json import JSONDump
 from litescope.software.dump.sigrok import SigrokDump
 from litescope.software.dump.vcd import VCDDump
