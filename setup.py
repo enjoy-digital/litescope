f23fac9a3232a4ee30050acbc47c3ac28d582616
Florent Kermarrec <florent@enjoy-digital.fr>
2023-12-19 10:04:29 +0100

    setup.py: Improve indentation.

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ from setuptools import find_packages
 
 
 setup(
-    name="litescope",
-    description="Small footprint and configurable embedded FPGA logic analyzer core",
-    author="Florent Kermarrec",
-    author_email="florent@enjoy-digital.fr",
-    url="http://enjoy-digital.fr",
-    download_url="https://github.com/enjoy-digital/litescope",
-    test_suite="test",
-    license="BSD",
-    python_requires="~=3.6",
-    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
-    include_package_data=True,
+    name                 = "litescope",
+    description          = "Small footprint and configurable embedded FPGA logic analyzer core",
+    author               = "Florent Kermarrec",
+    author_email         = "florent@enjoy-digital.fr",
+    url                  = "http://enjoy-digital.fr",
+    download_url         = "https://github.com/enjoy-digital/litescope",
+    test_suite           = "test",
+    license              = "BSD",
+    python_requires      = "~=3.6",
+    packages             = find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data = True,
     entry_points={
         "console_scripts": [
             "litescope_cli=litescope.software.litescope_cli:main",
