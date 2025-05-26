3898771a9d1cff274e30fdd81883bb43f837f853
Florent Kermarrec <florent@enjoy-digital.fr>
2025-05-26 10:36:02 +0200

    setup.py: Bump to 2025.04.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 setup(
     name                          = "litescope",
-    version                       = "2024.12",
+    version                       = "2025.04",
     description                   = "Small footprint and configurable embedded FPGA logic analyzer core",
     long_description              = long_description,
     long_description_content_type = "text/markdown",
